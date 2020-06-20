5ffbddcf6a874eeacee3f096f7982874cf902ac8
snakindia <deepakb@softclouds.com>
2020-06-20 18:12:48 +0530

    Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Client from './Home/Client';
 import ProductsPart from './Home/ProductsParts';
+import OEM from './Home/OEM';
+import Banner from './Home/Banner';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <Banner />
+        <OEM />
         <ProductsPart />
         <Client />
       </div>
