3e8f0aa9b15192abd9f84db855675def4adba03a
Ritesh Kumar <ritesh.prog@gmail.com>
2020-06-20 17:46:32 +0530

    onflicts

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
-
-<<<<<<< HEAD
 import "./index.css";
 import "./assets/css/layout.css";
 import "./assets/css/reset.css";
@@ -12,6 +10,7 @@ import "./assets/css/imagehover.css";
 import "./assets/css/slick.css";
 import "./assets/css/slick-theme.css";
 import "./assets/css/webslidemenu.css";
+
 import App from "./App";
 
 import Routes from './routes'
