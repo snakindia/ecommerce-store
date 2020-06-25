05ad9ce55838a079b00e5496662d732a1fabdbac
Ritesh Kumar <ritesh.prog@gmail.com>
2020-06-25 17:29:43 +0530

    home page image slider dynamic

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -4,54 +4,60 @@ import Image from '../../assets/images/2000X500.jpg';
 import FiveImage from '../../assets/images/1400X500-2.jpg';
 import RBCarousel from "react-bootstrap-carousel";
 import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
-
+import {API_URL} from '../../constants/appConstant'
+import axios from 'axios';
 export default class Banner extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            DynamicSlider: [],
+            autoplay: false,
+        }
+    }
+
+    componentDidMount() {
+        axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+        axios.get(API_URL+"/theme/settings")
+          .then( response => {
+            const DynamicSlider = response.data
+            console.log(DynamicSlider)
+            this.setState({DynamicSlider});
+            
+          })
+        }
+
   render() {
     this.slider = React.createRef();
-    this.state = {
-        autoplay: false
-    };
+    const {home_slider} = this.state.DynamicSlider
     
     return (
-        <div className="banner-container">
-            <div id="carousel" className="carousel slide" data-ride="carousel">
-                <div className="carousel-inner" role="listbox">
-                    <RBCarousel
+            <div className="banner-container">
+                <div id="carousel" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner" role="listbox">
+                        <RBCarousel
                         autoplay={this.state.autoplay}
                         pauseOnVisibility={true}
                         slideshowSpeed={2000}
                         version={4}
-                    >
-                        <div>
-                            <picture>
-                                  <img src={Image} alt="responsive image" className="d-block img-fluid" />
-                            </picture>
-                            <div className="caption">
-                                <h1 className="bha_heading_1">dust collector EQUIPMENT & Speare parts</h1>
-                                <a href="#" className="btn bha-btn-primary mt-4 pl-pr">view details</a>
-                            </div>
-                        </div>
 
-                        <div>
-                            <picture>
-                                <img src={PexelPhotoImage} alt="responsive image" className="d-block img-fluid" />
-                              </picture>
-                            <div className="caption">
-                                <h1 className="bha_heading_1">dust collector EQUIPMENT & Speare parts</h1>
-                                <a href="#" className="btn bha-btn-primary mt-4 pl-pr">view details</a>
-                            </div>
-                        </div>
-
-                        <div>
-                            <picture>
-                                <img src={FiveImage} alt="responsive image" className="d-block img-fluid" />
-                            </picture>
-                            <div className="caption">
-                                <h1 className="bha_heading_1">dust collector EQUIPMENT & Speare parts</h1>
-                                <a href="#" className="btn bha-btn-primary mt-4 pl-pr">view details</a>
-                            </div>
-                        </div>
-                    </RBCarousel>
+                        >
+                        {
+                         home_slider && home_slider.map((item,i)=>{
+                           return (
+                              <div key={i+"aa"}>
+                                <picture>
+                                 <img src={item.image} alt="responsive image" className="d-block img-fluid" />
+                                </picture>
+                                <div className="caption">
+                                  <h1 className="bha_heading_1">{item.description}</h1>
+                                  <a href={item.path} className="btn bha-btn-primary mt-4 pl-pr">view details</a>
+                                </div>
+                              </div>
+                               )
+                             })
+                        }
+          
+                       </RBCarousel>
                 </div> 
             </div>
         </div>
