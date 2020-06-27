57113bedd60aff93180f3e847038c32421c4a384
snakindia <deepakb@softclouds.com>
2020-06-28 02:16:41 +0530

    Worked on sticky header on home page

--- src/components/HeaderFull.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import Baghouselogo from '../assets/images/baghouse_logo.svg'
-import {MDBIcon} from 'mdbreact';
-import NavBar from './NavBar';
-export default class HeaderFull extends Component {
-  render() {
-    
-  }
-}
