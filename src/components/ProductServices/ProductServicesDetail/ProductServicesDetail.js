31a2d18f03fe65d3a87b0efa4c48b5522d0140cb
deepak <deepakb@softclouds.com>
2020-09-16 16:40:44 +0530

    Changes in component for Bags page

--- src/components/ProductServices/ProductServicesDetail/ProductServicesDetail.js
+++ src/components/ProductServices/ProductServicesDetail/ProductServicesDetail.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {dateConversion} from './../../common/Util';
-import Clients from './../../Clients';
-import PremiumBrands from './../../Shop/PremiumBrands';
+import Clients from './../../Clients/ProductServicesClients';
+import PremiumBrands from './../../Shop/ProductServicesPremiumBrands';
 import baghouse1 from '../../../assets/images/baghouse1.png';
 import Goyen from '../../../assets/images/Goyen-Original.jpg';
 import shipping2 from '../../../assets/images/free-shipping2.png';
 import badge from '../../../assets/images/badge-28.png';
+import Best from '../../../assets/images/832c6ba1-best-icon.png';
 import './../../../assets/css/bha-landing.css';
 import { fetchProdcutServiceDetail } from './../productservice.actions';
 import ReactHtmlParser from 'react-html-parser';
@@ -95,148 +96,132 @@ class ProductServicesDetail extends Component {
             </div>
         </section> : ''
     }
-<section class="how-best-container">
-  <div class="bg-opeque">
-    <div class="container-fluid pt-4 pb-4">
-      <h2 class="bha_heading_2 z-index text-blue">How Best We Are</h2>
-    </div>
-  </div>
- 
-  <div class="table-responsive">
-    <article class="course-pricing-table">
-    <ul class="course-pricing-table">
-      <li class="bg-green course-pricing-table">
-        <button>Cycles</button>
-      </li>
-      <li class="bg-green course-pricing-table">
-        <button>Max PSI</button>
-      </li>
-      <li class="bg-green course-pricing-table active">
-        <button>Price</button>
-      </li>
-      <li class="bg-green course-pricing-table">
-        <button>Rating</button>
-      </li>
-      <li class="bg-green course-pricing-table">
-        <button>Contact</button>
-      </li>
-      <li class="bg-green course-pricing-table">
-        <button>Satisfaction Guarantee</button>
-      </li>
-      <li class="bg-green course-pricing-table">
-        <button>Lead Time</button>
-      </li>
-    </ul>  
+    <section class="how-best-container">
+        <div class="text-center">
+            <div class="container-fluid pt-5">
+                <h2 class="bha_heading_2 z-index text-blue">{details.contents[1].title}</h2>
+            </div>
+        </div>
+  
+        <div class="pagewrap">
+          <div class="text-center">
+            <div class="container content-padding">
+              <div class="row align-items-center card-gutter">
+                <div class="col-lg-4">
+                  <div class="card-1 mx-auto mt-2 border-blue">
+                    <div class="card-body p-0 pt-3 pb-3">
+                      <img src={Goyen} class="card-img-fit fade" alt="..." />
+                      <h3 class="card-name">Valves & Repair Kits</h3>
+                      <h2 class="text-uppercase font-weight-bold mb-4 text-blue">Others</h2>
+                      <ul class="list-unstyled">
+                        <li class="card-list-item">Cycles:<span class="font-weight-bold ml-3">500K</span></li>
+                        <li class="card-list-item">Max PSI:<span class="font-weight-bold ml-3">80</span></li>
+                        <li class="card-list-item">Price: <span class="font-weight-bold ml-3">$</span></li> 
+                        <li class="card-list-item">
+                          <div class="star-rating">
+                            <ul class="list-inline">
+                              <span class="mr-3 ml-0 pl-0">Rating:</span>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                            </ul>
+                          </div>
+                        </li>
+                        <li class="card-list-item">Contact:<span class="font-weight-bold ml-3">Not Available</span></li>
+                        <li class="card-list-item">Satisfaction Guarantee:
+                          <span class="font-weight-bold ml-3">No Returns</span></li>
+                        <li class="card-list-item">Lead Time:<span class="font-weight-bold ml-3">17 days</span></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
 
-    <table class="course-pricing-table shadow bottom">
-      <thead class="course-pricing-table">
-        <tr class="course-pricing-table">
-          <th class="hide course-pricing-table"></th>
-          <th class="hide course-pricing-table"></th>
-          <th class="bg-green course-pricing-table">Cycles</th>
-          <th class="bg-green course-pricing-table">Max PSI</th>
-          <th class="bg-green course-pricing-table default">Price</th>
-          <th class="bg-green course-pricing-table ">Rating</th>
-          <th class="bg-green course-pricing-table">Contact</th>
-          <th class="bg-green course-pricing-table">Satisfaction Guarantee</th>
-          <th class="bg-green course-pricing-table">Lead Time</th>
-        </tr>
-      </thead>
-      <tbody class="course-pricing-table">
-          <tr class="course-pricing-table bg-gray">
-          <td class="course-pricing-table">&nbsp;</td>
-          <td class="course-pricing-table head-blue">
-            <span>Valves & Repair Kits</span><div>Others</div>
-          </td>
-          <td class="course-pricing-table">500K</td>
-          <td class="course-pricing-table">80K</td>
-          <td class="course-pricing-table default">$</td>
-          <td class="course-pricing-table">
-            <div class="star-rating mt-3">
-              <ul class="list-inline">
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-              </ul>
+                <div class="col-lg-4">
+                  <div class="card-2 mx-auto border-red" style={{"margin-bottom": "2.2rem"}}>
+                    <img src={Best} class="sticker" alt="..." />
+                    <div class="card-body p-0 pb-3 pt-2">
+                      <img src={baghouse1} class="card-img-fit" alt="..." />
+                      <h3 class="card-name">Valves & Repair Kits</h3>
+                      <h2 class="text-uppercase font-weight-bold mb-4 text-blue">Baghouse America</h2>
+                      <ul class="list-unstyled">
+                        <li class="card-list-item">Cycles: <span class="font-weight-bold ml-3">2 Million</span></li>
+                        <li class="card-list-item">Max PSI: <span class="font-weight-bold ml-3">125</span></li>
+                        <li class="card-list-item">Price: <span class="font-weight-bold ml-3">$$</span></li>
+                        <li class="card-list-item">
+                          <div class="star-rating">
+                            <ul class="list-inline">
+                              <span class="mr-3 ml-0 pl-0">Rating:</span>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                            </ul>
+                          </div>
+                        </li>
+                        <li class="card-list-item">Contact: <span class="font-weight-bold ml-3">Order Online or Call for Pricing</span></li>
+                        <li class="card-list-item">Satisfaction Guarantee: <span class="font-weight-bold ml-3">Yes</span></li>
+                        <li class="card-list-item">Lead Time: <span class="font-weight-bold ml-3">In Stock</span></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-lg-4">
+                  <div class="card-3 mx-auto border-blue mt-2">
+                    <div class="card-body p-0 pt-3 pb-3">
+                      <img src={Goyen} class="card-img-fit" alt="..." />
+                      <h3 class="card-name">Valves & Repair Kits</h3>
+                      <h2 class="text-uppercase font-weight-bold mb-4 text-blue">Goyen</h2>
+                      <ul class="list-unstyled">
+                        <li class="card-list-item">Cycles: <span class="font-weight-bold ml-3">2 Million</span></li>
+                        <li class="card-list-item">Max PSI: <span class="font-weight-bold ml-3">125</span></li>
+                        <li class="card-list-item">Price: <span class="font-weight-bold ml-3">$$$$</span></li>
+                        <li class="card-list-item">
+
+                          <div class="star-rating">
+                            <ul class="list-inline">
+                              <span class="mr-3 ml-0 pl-0">Rating:</span>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                              <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                            </ul>
+                          </div>
+                        </li>
+                         <li class="card-list-item">Contact: <span class="font-weight-bold ml-3">Call 888-405-1143 for Pricing</span></li>
+                        <li class="card-list-item">Satisfaction Guarantee: <span class="font-weight-bold ml-3">Re-Stocking Fee</span></li>
+                        <li class="card-list-item">Lead Time: <span class="font-weight-bold ml-3">In Stock</span></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </td>
-          <td class="course-pricing-table">Not Available</td>
-          <td class="course-pricing-table">No Returns</td>
-          <td class="course-pricing-table">17 days</td>
-        </tr>
-        <tr class="bg-orange-light">
-          <td class="course-pricing-table bg-white">
-            <div class="corner-tag">
-              <div class="corner"><span>Best<br/>Value</span></div>
+          </div>
+        </div>
+  
+  
+  
+        <div class="certificate-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4 col-md-4 text-center">
+                        <img src={shipping2} class="img-fluid" alt="..." />
+                    </div>
+                    <div class="col-sm-4 col-md-4 text-center">
+                        <img src={badge} class="img-fluid" alt="..." />
+                    </div>
+                    <div class="col-sm-4 col-md-4 text-center">
+                        <img src={badge} class="img-fluid" alt="..." />
+                    </div>
+                </div>
             </div>
-            <img src={baghouse1} alt="..." class="img-fluid" />
-          </td>
-          <td class="course-pricing-table head-orange">
-            <span>Valves & Repair Kits</span>
-            <div>baghouse america</div>
-          </td>
-          <td class="course-pricing-table">2 Million</td>
-          <td class="course-pricing-table">110</td>
-          <td class="course-pricing-table default">$$</td>
-          <td class="course-pricing-table">
-            <div class="star-rating mt-3">
-              <ul class="list-inline">
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-              </ul>
-            </div>
-          </td>
-          <td class="course-pricing-table">Order Online or Call for Pricing</td>
-          <td class="course-pricing-table">Yes</td>
-          <td class="course-pricing-table">In Stock</td>
-        </tr>
-        <tr class="course-pricing-table bg-white">
-          <td class="course-pricing-table bg-white">
-            <img src={Goyen} alt="..." class="img-fluid" />
-          </td>
-          <td class="course-pricing-table head-red">
-            <span>Valves & Repair Kits</span><div>goyens</div>
-          </td>
-          <td class="course-pricing-table">2 Million</td>
-          <td class="course-pricing-table">115</td>
-          <td class="course-pricing-table default">$$$</td>
-          <td class="course-pricing-table">
-            <div class="star-rating mt-3">
-              <ul class="list-inline">
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                <li class="list-inline-item"><i class="fa fa-star"></i></li>
-              </ul>
-            </div>
-          </td>
-          <td class="course-pricing-table">Call for Pricing</td>
-          <td class="course-pricing-table">Re-Stocking Fee</td>
-          <td class="course-pricing-table">In Stock</td>
-        </tr>
-      </tbody>
-    </table>
-  </article>
-  </div>
-  <div class="certificate-container">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-4 col-md-4 text-center">
-        <img src={shipping2} class="img-fluid" alt="..." />
-      </div>
-      <div class="col-sm-4 col-md-4 text-center">
-        <img src={badge} class="img-fluid" alt="..." />
-      </div>
-      <div class="col-sm-4 col-md-4 text-center">
-        <img src={badge} class="img-fluid" alt="..." />
-      </div>
-    </div>
-  </div>
-</div>
-</section>
+        </div>
+    </section>
 
     {
     details.contents && Object.keys(details.contents).length == 3 ?  
---
+++ src/components/Shop/ProductServicesPremiumBrands.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { API_URL, API_IMAGE_PATH } from './../../constants/appConstant';
+import parseHtml from 'react-html-parser';
+
+export default class ProductServicesPremiumBrands extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brands: {},
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + '/get_branded_category_list?fields=name,image,description')
+      .then(res => {
+        this.setState({ brands: res.data });
+      });
+  }
+
+  render() {
+    const { brands } = this.state;
+
+    var settings = {
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+    };
+
+    return (
+        <section class="shopping-product">
+            <div class="bg-opeque">
+                <div class="container-fluid">
+                    <h2 class="bha_heading_2 z-index text-blue">Shop by premium brands</h2>
+                </div>
+            </div>
+            <div class="pagewrap">
+                <section class="grid-container mt-4">
+                   <div class="container-fluid pl-0 pr-0">
+                      <div class="row">
+                        {Object.keys(brands).length &&
+                          brands.map((item, idx) => {
+                            return (
+                              <div class="col-sm-4 col-md-4 bha-mb">
+                                <a href="#">
+                                  <div
+                                    class="view view-first height-auto"
+                                  >
+                                    <img
+                                      class="img-fluid"
+                                      src={
+                                        item.image.length > 0
+                                          ? item.image
+                                          : API_IMAGE_PATH + 'default/default.jpg'
+                                      }
+                                      alt="Card image"
+                                    />
+                                    <div class="brand-label">{item.name}</div>
+                                    <div class="mask">
+                                      <p>{parseHtml(item.description)}</p>
+                                    </div>
+                                  </div>
+                                </a>
+                              </div>
+                            );
+                          })}
+                      </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+    );
+  }
+}
