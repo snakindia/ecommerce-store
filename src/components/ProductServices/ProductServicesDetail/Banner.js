2928c19ed02f57ed166f889a79263a2ebcee2ae6
Naveen Sharma <naveens@snakindia.com>
2020-09-21 17:35:40 +0530

    Ui Fixes

--- src/components/ProductServices/ProductServicesDetail/Banner.js
+++ src/components/ProductServices/ProductServicesDetail/Banner.js
@@ -3,12 +3,17 @@ import ContactForm from '../../common/ContactForm';
 
 const Banner = ({ content }) => {
   return (
-        <section class="hero-header" style={{"float":"left", "width": "100%", "padding-top": "150px"}}>
-            <div class="pagewrap">
+        <section class="hero-header" style={{"background": "none"}}>
+            <img src={content.banner_image} alt="..." class="img-fluid banner-img-100"/>
+            <div class="pagewrap" style={{"position": "absolute", "top":"35%"}}>
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-sm-8 col-md-8 pt-5 pl-0" style={{"position": "relative"}}>
-                            <img src={content.banner_image} alt="..." class="img-object" />
+                            
+                            <div style={{"position": "absolute", "top":"5%", "right":"30px", "width": "250px"}}>
+                            <div class="type1">GET UP TO</div><div class="type2"> 60% OFF</div> 
+                            <div class="type3">TODAY!</div>
+                            </div>
                         </div>
                         <div class="col-sm-5 col-md-4 pr-0">
                             <div class="freequote-container">
@@ -16,8 +21,6 @@ const Banner = ({ content }) => {
                                     <a href="#">Download Full Brochure</a>
                                 </div>
                                 <ContactForm />
-
-
                             </div>
                         </div>
                     </div>
