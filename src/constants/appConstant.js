91d1bde1ac9ebd808388f394d26291c7369e68fd
Naveen Sharma <naveens@snakindia.com>
2020-09-21 12:23:31 +0530

    API Fixes

--- src/constants/appConstant.js
+++ src/constants/appConstant.js
@@ -1,8 +1,8 @@
-const url = window.localStorage.__url || '49.50.102.36'; // 127.0.0.1
+const url = window.localStorage.__url || '209.59.154.198'; // 127.0.0.1
 
-const API_URL = 'http://49.50.102.36:3001/api/v1';
-const API_AJAX_URL = 'http://49.50.102.36:3001/ajax';
-const categoryImageUrl = 'http://49.50.102.36:3001/images/categories';
-const API_IMAGE_PATH = 'http://49.50.102.36:3001/images/';
+const API_URL = 'http://209.59.154.198:3001/api/v1';
+const API_AJAX_URL = 'http://209.59.154.198:3001/ajax';
+const categoryImageUrl = 'http://209.59.154.198:3001/images/categories';
+const API_IMAGE_PATH = 'http://209.59.154.198:3001/images/';
 
 export { API_URL, API_AJAX_URL, categoryImageUrl, API_IMAGE_PATH };
