3277ecf3fe3bc6549b5c2b9cbc85772adb3f0b4d
deepak <deepakb@softclouds.com>
2020-10-06 15:47:33 +0530

    Bug Fixes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -283,7 +283,7 @@ class About extends React.Component {
                               </div>
                               <div class="row">
                                 <div class="col-lg-12" style={{"padding":"0 2rem 1rem"}}>
-                                    <p class="text-left" style={{"font-size":"1rem"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                    <p class="text-left" style={{"font-size":"1rem"}}> {htmlParse(item.description)}</p>
                                 </div>
                               </div>
                             </div>
