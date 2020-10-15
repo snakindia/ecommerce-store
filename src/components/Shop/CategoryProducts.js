8f76f6ebb47a05429ee2884dcb1ef6320f376536
lakhvinder <lkm1developer@gmail.com>
2020-10-15 11:44:48 +0530

    no record

--- src/components/Shop/CategoryProducts.js
+++ src/components/Shop/CategoryProducts.js
@@ -113,112 +113,118 @@ class CategoryProducts extends Component {
     const total = data && data.data ? data.data.length : 0;
     return (
       <section className="pro-equipment-section">
-        <div className="container shorting-box border-bottom">
-          <div className="row">
-            <div className="col-sm-5 col-md-5 pl-0 pr-0">
-              <div className="short-items">
-                <span>Sort By:</span>
-                <Select
-                  showSearch={false}
-                  style={{ width: 200 }}
-                  onChange={this.sort}
-                  defaultValue={sorting}
-                >
-                  <Option value='latest'>Newest Items</Option>
-                  <Option selected="bestselling" value="bestselling">Bestselling</Option>
-                  <Option value="AZ">Alphabetical: A to Z</Option>
-                  <Option value="ZA">Alphabetical: Z to A</Option>
-                  <Option value="avgcustomerreview">Avg. Customer Review</Option>
-                  <Option value="priceasc">Price: Low to High</Option>
-                  <Option value="pricedesc">Price: High to Low</Option>
-
-                </Select>
-
-              </div>
-            </div>
-            <div className="col-sm-7 col-md-7 pl-0 pr-0">
-              <div className="w-100">
-                <Link type="button" onClick={e => this.setView(e, 'list')} className="grid-switcher" title="List View"><i className="fas fa-list"></i></Link>
-                <Link type="button" onClick={e => this.setView(e, 'grid')} className="grid-switcher" title="Grid View"><i className="fas fa-th"></i></Link>
-                <Pagination
-                  current={currentPage}
-                  total={total}
-                  size={resultPerPage}
-                  onChange={this.changePagination}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
         {
-          view == 'grid' ?
-            <div className="container-fluid pl-0 pr-0 product-xs-item">
-              <div className="row no-gutters">
-                {products.map((item, idx) =>
-                  <div className="col-sm-6 col-md-6 col-lg-3 border-bottom">
-                    <Product
-                      item={item}
-                      quickView={this.props.quickView}
+          total && total > 0 ? <>
+
+            <div className="container shorting-box border-bottom">
+              <div className="row">
+                <div className="col-sm-5 col-md-5 pl-0 pr-0">
+                  <div className="short-items">
+                    <span>Sort By:</span>
+                    <Select
+                      showSearch={false}
+                      style={{ width: 200 }}
+                      onChange={this.sort}
+                      defaultValue={sorting}
+                    >
+                      <Option value='latest'>Newest Items</Option>
+                      <Option selected="bestselling" value="bestselling">Bestselling</Option>
+                      <Option value="AZ">Alphabetical: A to Z</Option>
+                      <Option value="ZA">Alphabetical: Z to A</Option>
+                      <Option value="avgcustomerreview">Avg. Customer Review</Option>
+                      <Option value="priceasc">Price: Low to High</Option>
+                      <Option value="pricedesc">Price: High to Low</Option>
+
+                    </Select>
+
+                  </div>
+                </div>
+                <div className="col-sm-7 col-md-7 pl-0 pr-0">
+                  <div className="w-100">
+                    <Link type="button" onClick={e => this.setView(e, 'list')} className="grid-switcher" title="List View"><i className="fas fa-list"></i></Link>
+                    <Link type="button" onClick={e => this.setView(e, 'grid')} className="grid-switcher" title="Grid View"><i className="fas fa-th"></i></Link>
+                    <Pagination
+                      current={currentPage}
+                      total={total}
+                      size={resultPerPage}
+                      onChange={this.changePagination}
                     />
                   </div>
-                )
-                }
-              </div>
-            </div> :
-            <div className="container-fluid pl-0 pr-0 product-xs-list">
-              <div className="row">
-                <div className="col-lg-12">
-                  <ul className="list-group">
-                    {products.map((item, idx) =>
-
-                      <ProductList
-                        item={item}
-                        quickView={this.props.quickView}
-                      />
-
-                    )
-                    }
-                  </ul>
                 </div>
               </div>
             </div>
+            {
+              view == 'grid' ?
+                <div className="container-fluid pl-0 pr-0 product-xs-item">
+                  <div className="row no-gutters">
+                    {products.map((item, idx) =>
+                      <div className="col-sm-6 col-md-6 col-lg-3 border-bottom">
+                        <Product
+                          item={item}
+                          quickView={this.props.quickView}
+                        />
+                      </div>
+                    )
+                    }
+                  </div>
+                </div> :
+                <div className="container-fluid pl-0 pr-0 product-xs-list">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <ul className="list-group">
+                        {products.map((item, idx) =>
+
+                          <ProductList
+                            item={item}
+                            quickView={this.props.quickView}
+                          />
+
+                        )
+                        }
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+            }
+
+
+            <div className="container box-shadow shorting-box">
+              <div className="row">
+                <div className="col-sm-4 col-md-4">
+                  <div className="short-items">
+                    <span>Show:</span>
+                    <Select
+                      showSearch={false}
+                      style={{ width: 200 }}
+                      onChange={this.changePageSize}
+                      defaultValue={this.state.resultPerPage}
+                    >
+                      <Option value={10}>10</Option>
+                      <Option value={20}>20</Option>
+                      <Option value={50}>50</Option>
+                      <Option value={100} >100</Option>
+                      <Option value={200}>200</Option>
+
+                    </Select>
+
+                  </div>
+                </div>
+                <div className="col-sm-8 col-md-8">
+                  <div className="w-100">
+
+                    <Pagination
+                      current={currentPage}
+                      total={total}
+                      size={resultPerPage}
+                      onChange={this.changePagination}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </> :
+            <h1 className="noprodct">No Product Found</h1>
         }
-
-
-        <div className="container box-shadow shorting-box">
-          <div className="row">
-            <div className="col-sm-4 col-md-4">
-              <div className="short-items">
-                <span>Show:</span>
-                <Select
-                  showSearch={false}
-                  style={{ width: 200 }}
-                  onChange={this.changePageSize}
-                  defaultValue={this.state.resultPerPage}
-                >
-                  <Option value={10}>10</Option>
-                  <Option value={20}>20</Option>
-                  <Option value={50}>50</Option>
-                  <Option value={100} >100</Option>
-                  <Option value={200}>200</Option>
-
-                </Select>
-
-              </div>
-            </div>
-            <div className="col-sm-8 col-md-8">
-              <div className="w-100">
-
-              <Pagination
-                  current={currentPage}
-                  total={total}
-                  size={resultPerPage}
-                  onChange={this.changePagination}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
       </section>
     )
   }
