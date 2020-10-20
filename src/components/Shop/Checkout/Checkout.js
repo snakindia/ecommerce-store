b1d1be9f091eb7155223e3136fc13e5c89421a20
lakhvinder <lkm1developer@gmail.com>
2020-10-20 17:42:26 +0530

    checkout done

--- src/components/Shop/Checkout/Checkout.js
+++ src/components/Shop/Checkout/Checkout.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 import { Collapse } from 'antd';
 import { Card } from 'antd';
 import Login from './Login'
+import Address from './Address'
+import Summary from './Summary';
 const { Panel } = Collapse;
 
 class Checkout extends Component {
@@ -16,8 +18,10 @@ class Checkout extends Component {
             showModal: false,
             item: null,
             visible: true,
-            email:'',
-            step:1
+            email: '',
+            step: 1,
+            shippingAddress: undefined,
+            billingAddress: undefined
         }
     }
 
@@ -26,18 +30,111 @@ class Checkout extends Component {
         // this.props.getProduct(id);
     }
 
-    setEmail =(email)=>{
-        this.setState({email})
+    setEmail = (email) => {
+        this.setState({ email })
     }
 
-    next =(step)=>{
-        this.setState({step})
+    next = (step) => {
+        this.setState({ step })
     }
+
+    shippingSave = (data, type) => {
+        console.log({ data, type });
+        if (type == 'shipping') {
+            if (data.same == 1) {
+                this.setState({
+                    shippingAddress: data,
+                    billingAddress: data,
+                    step: 4
+                })
+            } else {
+                this.setState({
+                    shippingAddress: data,
+                    step: 3
+                })
+            }
+
+        }
+        else if (type == 'billing') {
+            this.setState({
+                billingAddress: data,
+                step: 4
+            })
+        }
+
+    }
+
+    openPanel = (e) => {
+        const { email, billingAddress, shippingAddress } = this.state;
+        if (e && e.length > 1 && e[1]) {
+            const step = e[1];
+            if (step == 1) {
+                this.setState({ step })
+            }
+            else if (step == 2 && email) {
+                this.setState({ step })
+            } else if (step == 3 && shippingAddress) {
+                this.setState({ step })
+            } else if (step == 4 && billingAddress) {
+                this.setState({ step })
+            }
+        }
+        console.log({ e });
+    }
+
+
     render() {
-        const {authenticated,user}=this.props;
-        const {step}=this.state;
-
-
+        const { authenticated, user,cart } = this.props;
+        const { step, email, shippingAddress, billingAddress } = this.state;
+        const headerOne = <div className="headerone"><h5 className="hh1">1. Customer</h5>
+            {authenticated ? <div className="info">
+                <span>{email ? email : null}</span>
+                <button className="SignOut">Sign Out</button>
+            </div> : null}
+        </div>
+        let subtotal = 0;
+        let shipping = 0;
+        let tax = 0;
+        let total = 0;
+        let  dataSource =[];
+        if (Object.keys(cart) && Object.keys(cart).length > 0) {
+            for (const datum of Object.keys(cart)) {
+                if (cart[datum]) {
+                    
+                    const price = cart[datum].item.sale_price ? cart[datum].item.sale_price: cart[datum].item.regular_price;
+                    const qty = cart[datum].qty;
+                    subtotal = subtotal + (price * qty);
+                    const item ={
+                        name:cart[datum].item.name,
+                        qty:cart[datum].qty,
+                        price:`$${price}`
+                    };
+                    dataSource.push(item);
+                }
+            }
+        }
+        dataSource.push({
+            name:'',
+            qty:'Subtotal',
+            price:`$${subtotal}`
+        });
+        dataSource.push({
+            name:'',
+            qty:'Shipping',
+            price: shipping ? `$${shipping}`:'---'
+        });
+        dataSource.push({
+            name:'',
+            qty:'Tax',
+            price:tax ? `$${tax}`:'$0.00'
+        });
+    
+        total =subtotal+shipping+tax;
+        dataSource.push({
+            name:'Total',
+            qty:'',
+            price:total ? `$${total}`:'$0.00'
+        });
         return (
 
 
@@ -54,26 +151,26 @@ class Checkout extends Component {
                                                 <h1 className="bha_heading_2 text-blue padding-top30 padding-btm30">Check Out</h1>
                                                 <div id="accordion">
                                                     <Collapse
-                                                        defaultActiveKey={[step.toString()]}
-                                                    // onChange={callback}
+                                                        activeKey={step.toString()}
+                                                        onChange={this.openPanel}
                                                     >
-                                                        <Panel header="1. Customer" key="1">
-                                                            <Login 
-                                                            user={user}
-                                                             authenticated={authenticated}
-                                                             setEmail={this.setEmail}
-                                                             email={this.state.email}
-                                                             next ={this.next}
-                                                             />
-
+                                                        <Panel header={headerOne} key="1">
+                                                            {authenticated ? null : <Login
+                                                                user={user}
+                                                                authenticated={authenticated}
+                                                                setEmail={this.setEmail}
+                                                                email={this.state.email}
+                                                                next={this.next}
+                                                            />
+                                                            }
                                                         </Panel>
-                                                        <Panel header="2. Shipping" key="2">
-                                                            <p>dgfh</p>
+                                                        <Panel header="2. Shipping" key="2" >
+                                                            <Address type="shipping" submit={this.shippingSave} data={shippingAddress} />
                                                         </Panel>
-                                                        <Panel header="3. Billing" key="3" disabled>
-                                                            <p>dgfh</p>
+                                                        <Panel header="3. Billing" key="3" >
+                                                            <Address type="billing" submit={this.shippingSave} data={billingAddress} />
                                                         </Panel>
-                                                        <Panel header="4. Payment" key="4" disabled>
+                                                        <Panel header="4. Payment" key="4" >
                                                             <div className="card-body">
                                                                 <form>
                                                                     <div className="row">
@@ -104,7 +201,7 @@ class Checkout extends Component {
 
 
                                                                                 <div className="col-sm-12 col-md-12 mt-3">
-                                                                                    <h5>$75.70</h5>
+                                                                                    <h5>${total}</h5>
 
                                                                                     <div className="form-group mt-3 mb-3">
                                                                                         <a href="thank-you.html" className="btn bha-btn-primary margin-left-mobile15" name="buttonsubmit">Place Order</a>
@@ -124,106 +221,8 @@ class Checkout extends Component {
                                     <div className="col-sm-4 col-md-4">
 
                                         <div className="right-content aside">
-                                            <div id="accordion" className="myaccordion">
-                                                <div className="border-0">
-                                                    <div id="collapseTwo" className="collapse show" aria-labelledby="headingTwo" data-parent="#accordion">
-                                                        <div className="card-body card-body margin-top p-0">
-                                                            <div className="col-sm-12 col-md-12">
-                                                                <div className="row">
-                                                                    <div className="order-summary">
-                                                                        <div className="Heading">
-                                                                            Order Summary
-                                      <span><a href="cart.html">Edit Cart</a></span>
-                                                                        </div>
-
-                                                                        <div className="table-responsive">
-                                                                            <table className="table">
-                                                                                <thead>
-                                                                                    <tr>
-                                                                                        <th scope="col" width="60%"> Product Name</th>
-                                                                                        <th scope="col" width="10%">Quantity</th>
-                                                                                        <th scope="col" width="30%">Price</th>
-                                                                                    </tr>
-                                                                                </thead>
-                                                                                <tbody>
-                                                                                    <tr>
-                                                                                        <td className="no-border">Goyen® K0380 (M1131B) Replacement Repair Kit</td>
-                                                                                        <td className="no-border">01</td>
-                                                                                        <td className="no-border">$ 21.80</td>
-
-                                                                                    </tr>
-                                                                                    <tr>
-
-                                                                                        <td className="no-border">ASCO® C113826 Replacement Repair Kit for ASCO 1 1/2" Valve</td>
-                                                                                        <td className="no-border">01</td>
-                                                                                        <td className="no-border">$ 34.00</td>
-
-                                                                                    </tr>
-                                                                                    <tr>
-
-                                                                                        <td  >Goyen® K2529 Replacement Repair Kit</td>
-                                                                                        <td  >01</td>
-                                                                                        <td  >$ 19.90</td>
-
-                                                                                    </tr>
-
-                                                                                    <tr>
-
-                                                                                        <td  ></td>
-                                                                                        <td  ><strong>Subtotal</strong></td>
-                                                                                        <td  >$ 45874.70</td>
-
-                                                                                    </tr>
-                                                                                    <tr>
-
-                                                                                        <td className="no-border"  ></td>
-                                                                                        <td className="no-border"  ><strong>Shipping</strong></td>
-                                                                                        <td className="no-border"  >- - - -</td>
-
-                                                                                    </tr>
-                                                                                    <tr>
-
-                                                                                        <td  ></td>
-                                                                                        <td  ><strong>Tax</strong></td>
-                                                                                        <td  >$0.00</td>
-                                                                                    </tr>
-                                                                                    <tr>
-                                                                                        <td colSpan="3">
-                                                                                            <form>
-                                                                                                <div className="col-lg-12 padding-left0">
-                                                                                                    <div className="row">
-                                                                                                        <div className="col-sm-9 col-md-9 col-xs-12">
-                                                                                                            <div className="form-group mt-3">
-                                                                                                                <input type="text" className="form-control input-control" name="email" placeholder="Coupon/Gift Certificate code" />
-
-                                                                                                            </div>
-                                                                                                        </div>
-                                                                                                        <div className="col-sm-3 col-md-3 col-xs-12 padding-left0">
-                                                                                                            <div className="form-group mt-3">
-                                                                                                                <button type="button" className="btn bha-btn-apply margin-left-mobile15" name="buttonsubmit">Apply</button>
-                                                                                                            </div>
-                                                                                                        </div>
-                                                                                                    </div>
-                                                                                                </div>
-                                                                                            </form>
-                                                                                        </td>
-                                                                                    </tr>
-                                                                                    <tr>
-                                                                                        <td className="total-bg"  ><strong>Total</strong></td>
-                                                                                        <td className="total-bg"  ></td>
-                                                                                        <td className="total-bg"  >$ 75.70</td>
-                                                                                    </tr>
-                                                                                </tbody>
-                                                                            </table>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                            <Summary dataSource={dataSource}/>
+                                             </div>
                                     </div>
                                 </div>
                             </div>
@@ -242,7 +241,7 @@ class Checkout extends Component {
 }
 const mapStateToProps = (state) => ({
     loading: state.shop.loading,
-    product: state.shop.product,
+    cart: state.shop.cart,
     error: state.shop.error,
     authenticated: state.auth.authenticated,
     user: state.auth.customer_settings,
