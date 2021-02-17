19de0b8b625fc7e53eecae585a073659b170f816
MohitVema006 <mohitverma006@gmail.com>
2021-02-17 11:05:49 +0530

    product list update

--- src/components/Shop/ProductList.js
+++ src/components/Shop/ProductList.js
@@ -26,6 +26,7 @@ export default class ProductList extends Component {
             <li className="list-group-item">
                 <div className="media flex-column flex-lg-row p-3">
                     {/* <Favourite /> */}
+                    <div className="list-image-cls">
                     <div className="portfolio-hover">
                         <div className="portfolio-hover-content">
                             <div className="callToAction-list">
@@ -37,7 +38,8 @@ export default class ProductList extends Component {
                         </div>
                     </div>
                     <Image src={imageUrl} alt="Generic placeholder image" className="order-1 order-lg-1 mr-lg-4" width="200" />
-                    <div className="media-body order-2 order-lg-1">
+                    </div>
+                    <div className="media-body order-2 order-lg-1 pl-3">
                         <h6 className="mt-0 mb-2"><ToolTip text={item.name} length={1000} /></h6>
                         {/* <a href="product-details.html" className="text-muted mb-0"><ToolTip text={item.description} /></a> */}
                         <div className="d-flex align-items-center justify-content-left mt-1">
