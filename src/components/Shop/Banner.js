4f899fc67939c0ae1143a9654cae717ba13e2313
Naveen Sharma <naveens@snakindia.com>
2021-02-24 19:07:24 +0530

    Shop now banner UI fixes and implemented

--- src/components/Shop/Banner.js
+++ src/components/Shop/Banner.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import Pro01 from '../../assets/images/promo-banner/pro-01.jpg';
-import ProSecond from '../../assets/images/promo-banner/pro-02.jpg';
+import Pro01 from '../../assets/images/promo-banner/item5.jpg';
+import ProSecond from '../../assets/images/promo-banner/banner-small02.jpg';
+import ProThird from '../../assets/images/promo-banner/banner-small03.jpg';
+import imgSmallOne from '../../assets/images/promo-banner/600X5001.jpg';
+import imgSmallTwo from '../../assets/images/promo-banner/600X5002.jpg';
+import imgSmallThree from '../../assets/images/promo-banner/600X5003.jpg';
 import RBCarousel from 'react-bootstrap-carousel';
 
 class Banner extends Component {
@@ -8,7 +12,7 @@ class Banner extends Component {
       return (
         <div className="row">
           <div className="col-lg-12">
-            <div className="banner-container-xs mt-0">
+            <div className="banner-container-small mt-0">
               <RBCarousel
                 autoplay={true}
                 pauseOnVisibility={true}
@@ -17,35 +21,29 @@ class Banner extends Component {
                 className="item-xs"
               >
                 <div className="item-xs">
-                  <img src={Pro01} alt="" className="img-fluid" />
-                  <div className="banner-item-xs-caption">
-                    <h1 className="item_heading">Square Chuck Air Filter Cartridge</h1>
-                    <p className="item-description-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                    sed do eiusmod didunt ut labore et dolore magna aliqua.</p>
-                    <p className="item-price pt-3 mb-2">from<span className="priceTag">$399</span></p>
-                    <a href="#" data-toggle="modal" className="btn bha-btn-primary mt-2">Shop Now</a>
+                  <img src={Pro01} alt="" className="img-fluid mobileHide" />
+                  <img src={imgSmallOne} alt="" className="img-fluid mobileImg" />
+                  <div className="banner-item-xs-caption z-index">
+                    <h1 className="item_heading text-white">Dust Collection Equipment</h1>
+                    <a href="#" data-toggle="modal" className="btn bha-btn-primary btnLight mt-2">Read More</a>
                   </div>
                 </div>
   
                 <div className="item-xs">
-                  <img src={ProSecond} alt="" className="img-fluid" />
-                  <div className="banner-item-xs-caption">
-                    <h1 className="item_heading">Bin Vent Dust Collector</h1>
-                    <p className="item-description-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                    sed do eiusmod didunt ut labore et dolore magna aliqua.</p>
-                    <p className="item-price pt-3 mb-2">from<span className="priceTag">$399</span></p>
-                    <a href="#myModal" data-toggle="modal" className="btn bha-btn-primary mt-2">Shop Now</a>
+                  <img src={ProSecond} alt="" className="img-fluid mobileHide" />
+                  <img src={imgSmallTwo} alt="" className="img-fluid mobileImg" />
+                  <div className="banner-item-xs-caption z-index">
+                    <h1 className="item_heading text-white">Dust Collector Auxiliaries & Parts</h1>
+                    <a href="#myModal" data-toggle="modal" className="btn bha-btn-primary btnLight mt-2">Read More</a>
                   </div>
                 </div>
   
                 <div className="item-xs">
-                  <img src={Pro01} alt="" className="img-fluid" />
-                  <div className="banner-item-xs-caption">
-                    <h1 className="item_heading">Square Chuck Air Filter Cartridge</h1>
-                    <p className="item-description-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                                    sed do eiusmod didunt ut labore et dolore magna aliqua.</p>
-                    <p className="item-price pt-3 mb-2">from<span className="priceTag">$399</span></p>
-                    <a href="#myModal" data-toggle="modal" className="btn bha-btn-primary mt-2">Shop Now</a>
+                  <img src={ProThird} alt="" className="img-fluid mobileHide" />
+                  <img src={imgSmallThree} alt="" className="img-fluid mobileImg" />
+                  <div className="banner-item-xs-caption z-index">
+                    <h1 className="item_heading text-white">Brands & Replacements</h1>
+                    <a href="#myModal" data-toggle="modal" className="btn bha-btn-primary btnLight mt-2">Read More</a>
                   </div>
                 </div>
   
